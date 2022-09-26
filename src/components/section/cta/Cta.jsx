2dab43bdd0700e9a10d1b6c7e867c7ab7a9cc72c
junsungkim95 <junsungkim@hiblocks.io>
2022-09-26 15:33:38 +0900

    CTA 문구 변경

--- src/components/section/cta/Cta.jsx
+++ src/components/section/cta/Cta.jsx
@@ -17,18 +17,18 @@ const CTA = () => {
         ></SectionTitle>
         {lang === "Eng" ? (
           <p className="subtitle_eng">
-            MAGIC CLUB Open Commemorative Contest Pre-Registration
+            Coming Up: Magic Club NFT<br />marketplace Open Contest
           </p>
         ) : (
-          <p className="subtitle">매직클럽 오픈기념 컨테스트 사전예약 접수</p>
+          <p className="subtitle">매직클럽 NFT거래소 컨테스트 오픈예정</p>
         )}
         <div className="magic_club_v1_cta_content">
           <div className="join_comunity_btns">
             <Button disabled lg variant="mint" className="wishlist_btn">
             {lang === "Eng" ? 
-              "Starting October 4th"
+              "Coming Up"
               :
-              "10월 4일 오픈예정"
+              "오픈예정"
             }
             </Button>
           </div>
